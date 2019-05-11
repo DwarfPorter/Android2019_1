fe8b8b7660fd7850b9414627ba11b16de7238adb
DwarfPorter <mrvlmor@gmail.com>
2019-05-11 11:54:46 +0300

    HelloActivity: Ослабляем связь активити с логикой (факультатив)

--- Lesson-1/HelloActivity/app/src/main/java/ru/geekbrains/helloactivity/MainActivity.java
+++ Lesson-1/HelloActivity/app/src/main/java/ru/geekbrains/helloactivity/MainActivity.java
@@ -1,20 +1,43 @@
 package ru.geekbrains.helloactivity;
 
-import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {// Наследуем класс Activity
+public class MainActivity extends AppCompatActivity implements GreetingStrings {  // Наследуем класс Activity
     // или его потомка
     // Этот метод вызывается при создании Activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView greeting = (TextView) findViewById(R.id.greeting); // Получить элемент
-        BuilderGreetingPhrase builderGreetingPhrase = new BuilderGreetingPhrase(getResources());
-        greeting.setText(builderGreetingPhrase.get());
+        TextView greeting = findViewById(R.id.greeting); // Получить элемент
+        BuilderGreetingPhrase builderGreetingPhrase = new BuilderGreetingPhrase(this);
+        greeting.setText(builderGreetingPhrase.get());                            // Заменить значение
+    }
+
+    @Override
+    public String getHelloer() {
+        return getResources().getString(R.string.helloer);
+    }
+
+    @Override
+    public String getMorning() {
+        return getResources().getString(R.string.morning);
+    }
+
+    @Override
+    public String getAfternoon() {
+        return getResources().getString(R.string.afternoon);
+    }
+
+    @Override
+    public String getEvening() {
+        return getResources().getString(R.string.evening);
+    }
+
+    @Override
+    public String getNight() {
+        return getResources().getString(R.string.night);
     }
 }
