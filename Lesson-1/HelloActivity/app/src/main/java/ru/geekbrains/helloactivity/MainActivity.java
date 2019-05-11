4f5cf212ce1906c30b60e02ad7f9834b5c62cbe3
DwarfPorter <mrvlmor@gmail.com>
2019-05-11 11:44:53 +0300

    HelloActivity: Разбиваем приветствие по временам суток (факультатив)

--- Lesson-1/HelloActivity/app/src/main/java/ru/geekbrains/helloactivity/MainActivity.java
+++ Lesson-1/HelloActivity/app/src/main/java/ru/geekbrains/helloactivity/MainActivity.java
@@ -2,12 +2,29 @@ package ru.geekbrains.helloactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {// Наследуем класс Activity
+    // или его потомка
+    // Этот метод вызывается при создании Activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView greeting = (TextView) findViewById(R.id.greeting); // Получить элемент
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY); // час сейчас
+        if (5 <= currentHour && currentHour < 12 ){         // Если утро
+            greeting.setText("Good morning GeekBrains!");
+        }
+        else if (12 <= currentHour && currentHour < 6){     // Если день
+            greeting.setText("Good afternoon GeekBrains!");
+        }
+        else if (6 <= currentHour && currentHour < 9){      // Если вечер
+            greeting.setText("Good evening GeekBrains!");
+        }
+        else {
+            greeting.setText("Good night GeekBrains!");     // Если поздний вечер или ночь
+        }
     }
 }
