c33c2c29f73d4546be2d097738451ab753c7a85d
DwarfPorter <mrvlmor@gmail.com>
2019-05-12 20:01:54 +0300

    CheckEquality: Обработка исключения. Вывод пользователю указания.

--- Lesson-5/CheckEquality/app/src/main/java/ru/geekbrains/checkequality/MainActivity.java
+++ Lesson-5/CheckEquality/app/src/main/java/ru/geekbrains/checkequality/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 } catch (NumberFormatException e) {
                     e.printStackTrace();
+                    textView.setText("Введите число!");
                 }
             }
         });
