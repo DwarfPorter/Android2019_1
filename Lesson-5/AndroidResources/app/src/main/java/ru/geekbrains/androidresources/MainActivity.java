c19470acb31052d53ba90314942448eb1afe0f97
DwarfPorter <mrvlmor@gmail.com>
2019-05-12 21:25:43 +0300

    AndroidResources: Программное переопределение стиля

--- Lesson-5/AndroidResources/app/src/main/java/ru/geekbrains/androidresources/MainActivity.java
+++ Lesson-5/AndroidResources/app/src/main/java/ru/geekbrains/androidresources/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.SecondStyle);
         setContentView(R.layout.main);
     }
 }
